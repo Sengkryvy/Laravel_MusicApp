040a92d6c10c8cbe72849fa49c413d17e705bf82
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2019-07-03 16:16:56 +0700

    edite main.js

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
 
     //menu selection
     $("aside ul li a").on('click', function () {
-        alert("click");
         $("aside ul li, .active-menu").removeClass("active-menu");
         $(this).parent().addClass("active-menu");
         $(this).addClass("active-menu");
